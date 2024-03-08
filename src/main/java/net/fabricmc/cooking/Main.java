243e880fa50956fb17ca220f3c09cc8ed114b873
Lilly Hewitt <70710764+lillyhewitt@users.noreply.github.com>
2024-03-07 19:57:45 -0500

    Create Main.java

---
+++ src/main/java/net/fabricmc/cooking/Main.java
@@ -0,0 +1,21 @@
+package net.fabricmc.cooking;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.cooking.registry.ModBlocks;
+import net.fabricmc.cooking.registry.ModItems;
+
+
+public class Main implements ModInitializer {
+	//
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		ModItems.registerItems();
+		ModBlocks.registerItems();
+
+
+	}
+}
